fdf5fb991cc2879a96cc05a53fb729e2301a741b
aboursin <angelo.boursin@gmail.com>
2016-02-03 14:18:30 +0100

    Spring profiles

--- springldap/README.md
+++ springldap/README.md
@@ -9,17 +9,45 @@ This sample is a Directory portal based on [Spring LDAP].
 - [Spring MVC]
 - [Spring LDAP]
 - [Twitter Bootstrap]
+- [Bootstrap Dialog]
 - [jQuery DataTables]
 
+## Attributes mapping
 
-   [Spring MVC]: <http://projects.spring.io/spring-framework/>
-   [Spring LDAP]: <http://projects.spring.io/spring-ldap/>
-   [Twitter Bootstrap]: <http://twitter.github.com/bootstrap/>
-   [jQuery DataTables]: <http://datatables.net/>
-   
+Basically mapping is described with `@LdapAttribute` annotation.
+
+``` java
+@LdapAttribute("givenName")
+private String firstname;
+```
+
+## Profiles
+
+### std [default]
+This profile will directly connect to a remote LDAP using connection information from _ldap.properties_ file
+
+### demo
+This profile will start an embedded LDAP server ([Apache DS]) populated with _ldap.ldif_ file.
+
+Connection information for the embedded LDAP server :
+```
+ldap.url=ldap://127.0.0.1:389
+ldap.base=ou=users,o=sample
+ldap.userDn=
+ldap.password=
+```
+
 ## Run
 To run this sample :
 
 1. Do `mvn package` in order to build springldap.war
-2. Deploy the war on a Tomcat
-3. Navigate to `http://localhost:8080/springldap`
+2. Add `-Dspring.profiles.active=demo` to your Tomcat Java options (if you want to use embedded LDAP server)
+3. Deploy the war on a Tomcat
+4. Navigate to `http://localhost:8080/springldap`
+
+   [Spring MVC]: <http://projects.spring.io/spring-framework/>
+   [Spring LDAP]: <http://projects.spring.io/spring-ldap/>
+   [Twitter Bootstrap]: <http://twitter.github.com/bootstrap/>
+   [Bootstrap Dialog]: <https://nakupanda.github.io/bootstrap3-dialog/>
+   [jQuery DataTables]: <http://datatables.net/>
+   [Apache DS]: <https://directory.apache.org/apacheds/>
--- springldap/src/main/java/sample/springldap/configuration/LdapDemoConfiguration.java
+++ springldap/src/main/java/sample/springldap/configuration/LdapDemoConfiguration.java
@@ -9,9 +9,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.ldap.core.ContextSource;
 import org.springframework.ldap.core.LdapTemplate;
 import org.springframework.ldap.core.support.LdapContextSource;
+import org.springframework.ldap.support.LdapUtils;
 import org.springframework.ldap.test.LdapTestUtils;
 
 /**
@@ -19,17 +21,17 @@ import org.springframework.ldap.test.LdapTestUtils;
  * @see GlobalConfiguration
  * @author angelo.boursin
  */
-@Profile("std")
+@Profile("demo")
 @Configuration
 @PropertySource("classpath:ldap.properties")
-public class LdapConfiguration implements  DisposableBean {
+public class LdapDemoConfiguration implements  DisposableBean {
 
-	private static Logger LOGGER = LoggerFactory.getLogger(LdapConfiguration.class);
+	private static Logger LOGGER = LoggerFactory.getLogger(LdapDemoConfiguration.class);
 	
 	@Autowired
 	private Environment env;
 	
-	public LdapConfiguration(){
+	public LdapDemoConfiguration(){
 		super();
 		LOGGER.info("Starting...");
 	}
@@ -41,12 +43,21 @@ public class LdapConfiguration implements  DisposableBean {
 	 */
 	@Bean
 	public ContextSource contextSource() throws Exception{
+		
+		// For Demo purpose : start embedded LDAP Server
+		LdapTestUtils.startEmbeddedServer(389, env.getProperty("ldap.base"), "sample");
+		
+		// Build context source
 		LdapContextSource contextSource = new LdapContextSource();
 		contextSource.setUrl(env.getProperty("ldap.url"));
 		contextSource.setBase(env.getProperty("ldap.base"));
 		contextSource.setUserDn(env.getProperty("ldap.userDn"));
 		contextSource.setPassword(env.getProperty("ldap.password"));
 		contextSource.afterPropertiesSet();
+		
+		// For Demo purpose : populate LDAP from LDIF file
+		LdapTestUtils.cleanAndSetup(contextSource, LdapUtils.newLdapName(env.getProperty("ldap.base")), new ClassPathResource("annuaire.ldif"));
+		
 		return contextSource;
 	}
 	
